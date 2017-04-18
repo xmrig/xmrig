4acfb213b8d5f807c1cba8a76e42276db3afe380
XMRig <xmrig@bunkr.org>
2017-04-18 12:06:46 +0300

    Add xmr-stak-cpu algo as experimental, use --av=5.

--- algo/cryptonight/cryptonight.h
+++ algo/cryptonight/cryptonight.h
@@ -27,23 +27,19 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define MEMORY          (1 << 21) /* 2 MiB */
-#define MEMORY_M128I    (MEMORY >> 4) // 2 MiB / 16 = 128 ki * __m128i
+#define MEMORY          2097152 /* 2 MiB */
 #define ITER            (1 << 20)
 #define AES_BLOCK_SIZE  16
 #define AES_KEY_SIZE    32 /*16*/
 #define INIT_SIZE_BLK   8
 #define INIT_SIZE_BYTE  (INIT_SIZE_BLK * AES_BLOCK_SIZE) // 128
-#define INIT_SIZE_M128I (INIT_SIZE_BYTE >> 4) // 8
 
-#pragma pack(push, 1)
+
 union hash_state {
   uint8_t b[200];
   uint64_t w[25];
 };
-#pragma pack(pop)
 
-#pragma pack(push, 1)
 union cn_slow_hash_state {
     union hash_state hs;
     struct {
@@ -51,7 +47,6 @@ union cn_slow_hash_state {
         uint8_t init[INIT_SIZE_BYTE];
     };
 };
-#pragma pack(pop)
 
 
 struct cryptonight_ctx {
