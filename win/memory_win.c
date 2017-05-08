a2574e1b1bd8d03aab90e93f9c657d0e726d3d55
XMRig <xmrig@bunkr.org>
2017-05-08 21:41:27 +0300

    Added message if huge pages was enabled, but reboot required.

--- win/memory_win.c
+++ win/memory_win.c
@@ -122,6 +122,7 @@ static BOOL ObtainLockPagesPrivilege() {
         LSA_UNICODE_STRING str = StringToLsaUnicodeString(_T(SE_LOCK_MEMORY_NAME));
 
         if (LsaAddAccountRights(handle, user->User.Sid, &str, 1) == 0) {
+            applog_notime(LOG_WARNING, "Huge pages support was successfully enabled, but reboot required to use it");
             result = TRUE;
         }
 
