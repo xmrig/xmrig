1123c20da0fab7c08be877af4abf85ea63076f60
XMRig <support@xmrig.com>
2017-10-07 00:49:38 +0300

    Add comments to config file.

--- src/Options.cpp
+++ src/Options.cpp
@@ -55,36 +55,39 @@ Options *Options::m_self = nullptr;
 static char const usage[] = "\
 Usage: " APP_ID " [OPTIONS]\n\
 Options:\n\
-  -a, --algo=ALGO       cryptonight (default) or cryptonight-lite\n\
-  -o, --url=URL         URL of mining server\n\
-  -O, --userpass=U:P    username:password pair for mining server\n\
-  -u, --user=USERNAME   username for mining server\n\
-  -p, --pass=PASSWORD   password for mining server\n\
-  -t, --threads=N       number of miner threads\n\
-  -v, --av=N            algorithm variation, 0 auto select\n\
-  -k, --keepalive       send keepalived for prevent timeout (need pool support)\n\
-  -r, --retries=N       number of times to retry before switch to backup server (default: 5)\n\
-  -R, --retry-pause=N   time to pause between retries (default: 5)\n\
-      --cpu-affinity    set process affinity to CPU core(s), mask 0x3 for cores 0 and 1\n\
-      --cpu-priority    set process priority (0 idle, 2 normal to 5 highest)\n\
-      --no-huge-pages   disable huge pages support\n\
-      --no-color        disable colored output\n\
-      --donate-level=N  donate level, default 5%% (5 minutes in 100 minutes)\n\
-      --user-agent      set custom user-agent string for pool\n\
-  -B, --background      run the miner in the background\n\
-  -c, --config=FILE     load a JSON-format configuration file\n\
-  -l, --log-file=FILE   log all output to a file\n"
+  -a, --algo=ALGO          cryptonight (default) or cryptonight-lite\n\
+  -o, --url=URL            URL of mining server\n\
+  -O, --userpass=U:P       username:password pair for mining server\n\
+  -u, --user=USERNAME      username for mining server\n\
+  -p, --pass=PASSWORD      password for mining server\n\
+  -t, --threads=N          number of miner threads\n\
+  -v, --av=N               algorithm variation, 0 auto select\n\
+  -k, --keepalive          send keepalived for prevent timeout (need pool support)\n\
+  -r, --retries=N          number of times to retry before switch to backup server (default: 5)\n\
+  -R, --retry-pause=N      time to pause between retries (default: 5)\n\
+      --cpu-affinity       set process affinity to CPU core(s), mask 0x3 for cores 0 and 1\n\
+      --cpu-priority       set process priority (0 idle, 2 normal to 5 highest)\n\
+      --no-huge-pages      disable huge pages support\n\
+      --no-color           disable colored output\n\
+      --donate-level=N     donate level, default 5%% (5 minutes in 100 minutes)\n\
+      --user-agent         set custom user-agent string for pool\n\
+  -B, --background         run the miner in the background\n\
+  -c, --config=FILE        load a JSON-format configuration file\n\
+  -l, --log-file=FILE      log all output to a file\n"
 # ifdef HAVE_SYSLOG_H
 "\
-  -S, --syslog          use system log for output messages\n"
+  -S, --syslog             use system log for output messages\n"
 # endif
 "\
-      --max-cpu-usage=N maximum CPU usage for automatic threads mode (default 75)\n\
-      --safe            safe adjust threads and av settings for current CPU\n\
-      --nicehash        enable nicehash support\n\
-      --print-time=N    print hashrate report every N seconds\n\
-  -h, --help            display this help and exit\n\
-  -V, --version         output version information and exit\n\
+      --max-cpu-usage=N    maximum CPU usage for automatic threads mode (default 75)\n\
+      --safe               safe adjust threads and av settings for current CPU\n\
+      --nicehash           enable nicehash/xmrig-proxy support\n\
+      --print-time=N       print hashrate report every N seconds\n\
+      --api-port=N         port for the miner API\n\
+      --api-access-token=T access token for API\n\
+      --api-worker-id=ID   custom worker-id for API\n\
+  -h, --help               display this help and exit\n\
+  -V, --version            output version information and exit\n\
 ";
 
 
