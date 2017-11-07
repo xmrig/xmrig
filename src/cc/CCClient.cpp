28c94eac6a19fa285711a9711ce38096c670af10
BenDroid <ben@graef.in>
2017-11-07 20:11:23 +0100

    Added xmrigCC-common src

---
+++ src/cc/CCClient.cpp
@@ -0,0 +1,228 @@
+/* XMRig
+ * Copyright 2010      Jeff Garzik <jgarzik@pobox.com>
+ * Copyright 2012-2014 pooler      <pooler@litecoinpool.org>
+ * Copyright 2014      Lucas Jones <https://github.com/lucasjones>
+ * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
+ * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
+ * Copyright 2016-2017 XMRig       <support@xmrig.com>
+ * Copyright 2017-     BenDr0id    <ben@graef.in>
+ *
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <fstream>
+#include <3rdparty/rapidjson/stringbuffer.h>
+#include <3rdparty/rapidjson/prettywriter.h>
+#include <Platform.h>
+
+#include "CCClient.h"
+#include "App.h"
+#include "ControlCommand.h"
+
+#include "api/NetworkState.h"
+#include "log/Log.h"
+#include "workers/Workers.h"
+#include "workers/Hashrate.h"
+
+#if _WIN32
+#   include "winsock2.h"
+#else
+#   include "unistd.h"
+#endif
+
+
+CCClient *CCClient::m_self = nullptr;
+uv_mutex_t CCClient::m_mutex;
+
+CCClient::CCClient(const Options *options)
+    : m_options(options)
+{
+    uv_mutex_init(&m_mutex);
+
+    m_self = this;
+
+    std::string clientId;
+    if (m_options->ccWorkerId()) {
+        clientId = m_options->ccWorkerId();
+    } else{
+        char hostname[128];
+        memset(hostname, 0, sizeof(hostname));
+        gethostname(hostname, sizeof(hostname)-1);
+        clientId = std::string(hostname);
+    }
+
+    m_clientStatus.setClientId(clientId);
+
+    if (m_options->algoName() != nullptr) {
+        m_clientStatus.setCurrentAlgoName(m_options->algoName());
+    }
+
+    if (m_options->ccToken() != nullptr) {
+        m_authorization = std::string("Bearer ") + m_self->m_options->ccToken();
+    }
+
+    uv_timer_init(uv_default_loop(), &m_timer);
+    uv_timer_start(&m_timer, CCClient::onReport,
+                   static_cast<uint64_t>(m_options->ccUpdateInterval() * 1000),
+                   static_cast<uint64_t>(m_options->ccUpdateInterval() * 1000));
+}
+
+CCClient::~CCClient()
+{
+    uv_timer_stop(&m_timer);
+    m_self = nullptr;
+}
+
+void CCClient::updateHashrate(const Hashrate *hashrate)
+{
+    uv_mutex_lock(&m_mutex);
+
+    if (m_self) {
+        m_self->m_clientStatus.setHashrateShort(hashrate->calc(Hashrate::ShortInterval));
+        m_self->m_clientStatus.setHashrateMedium(hashrate->calc(Hashrate::MediumInterval));
+        m_self->m_clientStatus.setHashrateLong(hashrate->calc(Hashrate::LargeInterval));
+        m_self->m_clientStatus.setHashrateHighest(hashrate->highest());
+    }
+
+    uv_mutex_unlock(&m_mutex);
+}
+
+
+void CCClient::updateNetworkState(const NetworkState &network)
+{
+    uv_mutex_lock(&m_mutex);
+
+    if (m_self) {
+        m_self->m_clientStatus.setCurrentStatus(Workers::isEnabled() ? ClientStatus::RUNNING : ClientStatus::PAUSED);
+        m_self->m_clientStatus.setCurrentPool(network.pool);
+        m_self->m_clientStatus.setSharesGood(network.accepted);
+        m_self->m_clientStatus.setSharesTotal(network.accepted + network.rejected);
+        m_self->m_clientStatus.setHashesTotal(network.total);
+        m_self->m_clientStatus.setAvgTime(network.avgTime());
+    }
+    uv_mutex_unlock(&m_mutex);
+}
+
+void CCClient::publishClientStatusReport()
+{
+    std::string requestUrl = "/client/setClientStatus?clientId=" + m_self->m_clientStatus.getClientId();
+    std::string requestBuffer = m_self->m_clientStatus.toJsonString();
+
+    auto res = performRequest(requestUrl, requestBuffer, "POST");
+    if (!res) {
+        LOG_ERR("[CC-Client] error: unable to performRequest POST -> http://%s:%d%s",
+                m_self->m_options->ccHost(), m_self->m_options->ccPort(), requestUrl.c_str());
+    } else if (res->status != 200) {
+        LOG_ERR("[CC-Client] error: \"%d\" -> http://%s:%d%s", res->status,  m_self->m_options->ccHost(),
+                m_self->m_options->ccPort(), requestUrl.c_str());
+    } else {
+        ControlCommand controlCommand;
+        if (controlCommand.parseFromJsonString(res->body)) {
+            if (controlCommand.getCommand() == ControlCommand::START) {
+                if (!Workers::isEnabled()) {
+                    LOG_WARN("[CC-Client] Command: START received -> resume");
+                    Workers::setEnabled(true);
+                }
+            } else if (controlCommand.getCommand() == ControlCommand::STOP) {
+                if (Workers::isEnabled()) {
+                    LOG_WARN("[CC-Client] Command: STOP received -> pause");
+                    Workers::setEnabled(false);
+                }
+            } else if (controlCommand.getCommand() == ControlCommand::UPDATE_CONFIG) {
+                LOG_WARN("[CC-Client] Command: UPDATE_CONFIG received -> update config");
+                updateConfig();
+            } else if (controlCommand.getCommand() == ControlCommand::RESTART) {
+                LOG_WARN("[CC-Client] Command: RESTART received -> restart");
+                App::restart();
+            } else if (controlCommand.getCommand() == ControlCommand::SHUTDOWN) {
+                LOG_WARN("[CC-Client] Command: SHUTDOWN received -> shutdown");
+                App::shutdown();
+            }
+        } else {
+            LOG_ERR("[CC-Client] Unknown command received from CC Server.");
+        }
+    }
+}
+
+void CCClient::updateConfig()
+{
+    std::string requestUrl = "/client/getConfig?clientId=" + m_self->m_clientStatus.getClientId();
+    std::string requestBuffer;
+
+    auto res = performRequest(requestUrl, requestBuffer, "GET");
+    if (!res) {
+        LOG_ERR("[CC-Client] error: unable to performRequest GET -> http://%s:%d%s",
+                m_self->m_options->ccHost(), m_self->m_options->ccPort(), requestUrl.c_str());
+    } else if (res->status != 200) {
+        LOG_ERR("[CC-Client] error: \"%d\" -> http://%s:%d%s", res->status,  m_self->m_options->ccHost(),
+                m_self->m_options->ccPort(), requestUrl.c_str());
+    } else {
+        rapidjson::Document document;
+        if (!document.Parse(res->body.c_str()).HasParseError()) {
+            std::ofstream clientConfigFile(m_self->m_options->configFile());
+            if (clientConfigFile) {
+                rapidjson::StringBuffer buffer(0, 65536);
+                rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
+                writer.SetMaxDecimalPlaces(10);
+                document.Accept(writer);
+
+                clientConfigFile << buffer.GetString();
+                clientConfigFile.close();
+
+                LOG_WARN("[CC-Client] Config updated. -> restart");
+                App::restart();
+            } else {
+                LOG_ERR("[CC-Client] Not able to store client config to file %s.", m_self->m_options->configFile());
+            }
+        } else{
+            LOG_ERR("[CC-Client] Not able to store client config. received client config is broken!");
+        }
+    }
+}
+
+std::shared_ptr<httplib::Response> CCClient::performRequest(const std::string& requestUrl,
+                                                            const std::string& requestBuffer,
+                                                            const std::string& operation)
+{
+    httplib::Client cli(m_self->m_options->ccHost(), m_self->m_options->ccPort());
+
+    httplib::Request req;
+    req.method = operation.c_str();
+    req.path = requestUrl.c_str();
+    req.set_header("Host", "");
+    req.set_header("Accept", "*/*");
+    req.set_header("User-Agent", Platform::userAgent());
+    req.set_header("Accept", "application/json");
+    req.set_header("Content-Type", "application/json");
+
+    if (!m_self->m_authorization.empty()) {
+        req.set_header("Authorization", m_self->m_authorization.c_str());
+    }
+
+    if (!requestBuffer.empty()) {
+        req.body = requestBuffer.c_str();
+    }
+
+    auto res = std::make_shared<httplib::Response>();
+
+    return cli.send(req, *res) ? res : nullptr;
+}
+
+void CCClient::onReport(uv_timer_t *handle)
+{
+    if (m_self) {
+        m_self->publishClientStatusReport();
+    }
+}
---
+++ src/cc/Httpd.cpp
@@ -0,0 +1,238 @@
+/* XMRig
+ * Copyright 2010      Jeff Garzik <jgarzik@pobox.com>
+ * Copyright 2012-2014 pooler      <pooler@litecoinpool.org>
+ * Copyright 2014      Lucas Jones <https://github.com/lucasjones>
+ * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
+ * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
+ * Copyright 2016-2017 XMRig       <support@xmrig.com>
+ * Copyright 2017-     BenDr0id    <ben@graef.in>
+ *
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cstring>
+#include <microhttpd.h>
+#include <memory>
+
+#include "version.h"
+#include "Service.h"
+#include "Httpd.h"
+#include "log/Log.h"
+
+Httpd::Httpd(const Options *options)
+        : m_options(options)
+        , m_daemon(nullptr)
+{
+}
+
+bool Httpd::start()
+{
+    if (!m_options->ccPort()) {
+        return false;
+    }
+
+    m_daemon = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY, static_cast<uint16_t>(m_options->ccPort()), nullptr, nullptr, &Httpd::handler,
+                                this, MHD_OPTION_END);
+
+    if (!m_daemon) {
+        LOG_ERR("HTTP Daemon failed to start.");
+        return false;
+    } else {
+        LOG_INFO("%s Server started on Port: %d", APP_NAME, m_options->ccPort());
+    }
+
+    return true;
+}
+
+unsigned Httpd::tokenAuth(struct MHD_Connection* connection)
+{
+    if (!m_options->ccToken()) {
+        LOG_WARN("AccessToken not set. Access Granted!");
+        return MHD_HTTP_OK;
+    }
+
+    const char* header = MHD_lookup_connection_value(connection, MHD_HEADER_KIND, MHD_HTTP_HEADER_AUTHORIZATION);
+    if (m_options->ccToken() && !header) {
+        return MHD_HTTP_UNAUTHORIZED;
+    }
+
+    const size_t size = strlen(header);
+    if (size < 8 || strlen(m_options->ccToken()) != size - 7 || memcmp("Bearer ", header, 7) != 0) {
+        LOG_WARN("AccessToken wrong. Access Forbidden!");
+        return MHD_HTTP_FORBIDDEN;
+    }
+
+    return strncmp(m_options->ccToken(), header + 7, strlen(m_options->ccToken())) == 0 ? MHD_HTTP_OK : MHD_HTTP_FORBIDDEN;
+}
+
+unsigned Httpd::basicAuth(struct MHD_Connection* connection, std::string& resp)
+{
+    if (!m_options->ccAdminUser() || !m_options->ccAdminPass()) {
+        resp = std::string("<html><body\\>"
+                           "Please configure admin user and pass to view this Page."
+                           "</body><html\\>");
+
+        LOG_WARN("Admin user/password not set. Access Forbidden!");
+        return MHD_HTTP_FORBIDDEN;
+    }
+
+    const char* header = MHD_lookup_connection_value(connection, MHD_HEADER_KIND, MHD_HTTP_HEADER_AUTHORIZATION);
+    if (!header) {
+        return MHD_HTTP_UNAUTHORIZED;
+    }
+
+    char* user;
+    char* pass;
+
+    user = MHD_basic_auth_get_username_password(connection, &pass);
+
+    if (user == nullptr || strcmp(user, m_options->ccAdminUser()) != 0 ||
+        pass == nullptr || strcmp(pass, m_options->ccAdminPass()) != 0) {
+
+        LOG_WARN("Admin user/password wrong. Access Unauthorized!");
+        return MHD_HTTP_UNAUTHORIZED;
+    }
+
+    return MHD_HTTP_OK;
+}
+
+int Httpd::sendResponse(MHD_Connection* connection, unsigned status, MHD_Response* rsp, const char* contentType)
+{
+    if (!rsp) {
+        rsp = MHD_create_response_from_buffer(0, nullptr, MHD_RESPMEM_MUST_COPY);
+    }
+
+    MHD_add_response_header(rsp, "Content-Type", contentType);
+    MHD_add_response_header(rsp, "Access-Control-Allow-Origin", "*");
+    MHD_add_response_header(rsp, "Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+    MHD_add_response_header(rsp, "Access-Control-Allow-Headers", "Content-Type, Authorization");
+    MHD_add_response_header(rsp, "WWW-Authenticate", "Basic");
+    MHD_add_response_header(rsp, "WWW-Authenticate", "Bearer");
+
+    int ret = MHD_queue_response(connection, status, rsp);
+
+    MHD_destroy_response(rsp);
+
+    return ret;
+}
+
+
+int Httpd::handler(void* httpd, MHD_Connection* connection, const char* url, const char* method,
+                   const char* version, const char* upload_data, size_t* upload_data_size, void** con_cls)
+{
+    if (strcmp(method, MHD_HTTP_METHOD_OPTIONS) == 0) {
+        LOG_INFO("OPTIONS Requested");
+        return sendResponse(connection, MHD_HTTP_OK, nullptr, CONTENT_TYPE_HTML);
+    }
+
+    if (strcmp(method, MHD_HTTP_METHOD_GET) != 0 && strcmp(method, MHD_HTTP_METHOD_POST) != 0) {
+        LOG_ERR("HTTP_METHOD_NOT_ALLOWED");
+        return sendResponse(connection, MHD_HTTP_METHOD_NOT_ALLOWED, nullptr, CONTENT_TYPE_HTML);
+    }
+
+    if (strstr(url, "/client/")) {
+        unsigned status = static_cast<Httpd*>(httpd)->tokenAuth(connection);
+        if (status != MHD_HTTP_OK) {
+            return sendResponse(connection, status, nullptr, CONTENT_TYPE_JSON);
+        }
+    } else {
+        std::string resp;
+        unsigned status = static_cast<Httpd*>(httpd)->basicAuth(connection, resp);
+        if (status != MHD_HTTP_OK) {
+            MHD_Response* rsp = nullptr;
+            if (!resp.empty()) {
+                rsp = MHD_create_response_from_buffer(resp.length(), (void*)resp.c_str(), MHD_RESPMEM_MUST_COPY);
+            }
+            return sendResponse(connection, status, rsp, CONTENT_TYPE_HTML);
+        }
+    }
+
+    if (strcmp(method, MHD_HTTP_METHOD_GET) == 0) {
+        return handleGET(static_cast<Httpd*>(httpd), connection, url);
+    } else {
+        return handlePOST(static_cast<Httpd*>(httpd), connection, url, upload_data, upload_data_size, con_cls);
+    }
+
+    return MHD_NO;
+}
+
+int Httpd::handleGET(const Httpd* httpd, struct MHD_Connection* connection, const char* urlPtr)
+{
+    std::string resp;
+    std::string url(urlPtr);
+    std::string clientId;
+
+    const char* clientIdPtr = MHD_lookup_connection_value(connection, MHD_GET_ARGUMENT_KIND, "clientId");
+    if (clientIdPtr)
+    {
+        clientId = std::string(clientIdPtr);
+    }
+
+    unsigned status = Service::handleGET(httpd->m_options, url, clientId, resp);
+
+    MHD_Response* rsp = nullptr;
+    if (!resp.empty()) {
+        rsp = MHD_create_response_from_buffer(resp.length(), (void*) resp.c_str(), MHD_RESPMEM_MUST_COPY);
+    }
+
+    char* contentType;
+    if (url == "/") {
+        contentType = const_cast<char*>(CONTENT_TYPE_HTML);
+    } else {
+        contentType = const_cast<char*>(CONTENT_TYPE_JSON);
+    }
+
+    return sendResponse(connection, status, rsp, contentType);
+}
+
+int Httpd::handlePOST(const Httpd* httpd, struct MHD_Connection* connection, const char* urlPtr, const char* upload_data,
+                      size_t* upload_data_size, void** con_cls)
+{
+    auto* cc = (ConnectionContext*)* con_cls;
+    if (cc == nullptr) {
+        cc = new ConnectionContext();
+        *con_cls = (void*) cc;
+    } else {
+        if (*upload_data_size != 0) {
+            cc->data << std::string(upload_data, *upload_data_size);
+
+            *upload_data_size = 0;
+        } else {
+            std::string resp;
+            std::string url(urlPtr);
+            std::string clientId;
+
+            const char* clientIdPtr = MHD_lookup_connection_value(connection, MHD_GET_ARGUMENT_KIND, "clientId");
+            if (clientIdPtr)
+            {
+                clientId = std::string(clientIdPtr);
+            }
+
+            unsigned status = Service::handlePOST(httpd->m_options, url, clientId, cc->data.str(), resp);
+
+            MHD_Response* rsp = nullptr;
+            if (!resp.empty()) {
+                rsp = MHD_create_response_from_buffer(resp.length(), (void*) resp.c_str(), MHD_RESPMEM_MUST_COPY);
+            }
+
+            delete cc;
+            *con_cls = nullptr;
+
+            return sendResponse(connection, status, rsp, CONTENT_TYPE_JSON);
+        }
+    }
+
+    return MHD_YES;
+}
