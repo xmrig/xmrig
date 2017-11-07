e34269d71bc8e4344641bfd41256dade894b8617
BenDroid <ben@graef.in>
2017-11-07 19:58:24 +0100

    Added cpp-httplib as libcurl replacement

---
+++ src/3rdparty/cpp-httplib/example/hello.cc
@@ -0,0 +1,22 @@
+//
+//  hello.cc
+//
+//  Copyright (c) 2012 Yuji Hirose. All rights reserved.
+//  The Boost Software License 1.0
+//
+
+#include <httplib.h>
+using namespace httplib;
+
+int main(void)
+{
+    Server svr;
+
+    svr.get("/hi", [](const auto& req, auto& res) {
+        res.set_content("Hello World!", "text/plain");
+    });
+
+    svr.listen("localhost", 1234);
+}
+
+// vim: et ts=4 sw=4 cin cino={1s ff=unix
