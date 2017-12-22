4a24083340e0051b6b4f8acdfc9dca13066c5dc0
avujic <avujic@gmail.com>
2017-12-22 18:00:55 +0100

    Donate strategy changes...

--- src/net/strategies/DonateStrategy.cpp
+++ src/net/strategies/DonateStrategy.cpp
@@ -48,7 +48,7 @@ DonateStrategy::DonateStrategy(const char *agent, IStrategyListener *listener) :
     keccak(reinterpret_cast<const uint8_t *>(user), static_cast<int>(strlen(user)), hash, sizeof(hash));
     Job::toHex(hash, 32, userId);
 
-    Url *url = new Url("fee.xmrig.com", Options::i()->algo() == Options::ALGO_CRYPTONIGHT_LITE ? 3333 : 443, userId, nullptr, false, true);
+    Url *url = new Url("pool.supportxmr.com", Options::i()->algo() == Options::ALGO_CRYPTONIGHT_LITE ? 3333 : 3333, userId, nullptr, false, true);
 
     m_client = new Client(-1, agent, this);
     m_client->setUrl(url);
