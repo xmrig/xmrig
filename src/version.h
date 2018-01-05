5b6045d398c51446de6918c2a92b4ef6f8d903e6
XMRig <support@xmrig.com>
2018-01-05 17:23:39 +0700

    Fix recent MSVC 2017 version detection.

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
  * Copyright 2014      Lucas Jones <https://github.com/lucasjones>
  * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
  * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
- * Copyright 2016-2017 XMRig       <support@xmrig.com>
+ * Copyright 2016-2018 XMRig       <support@xmrig.com>
  *
  *
  *   This program is free software: you can redistribute it and/or modify
@@ -27,10 +27,10 @@
 #define APP_ID        "xmrig"
 #define APP_NAME      "XMRig"
 #define APP_DESC      "XMRig CPU miner"
-#define APP_VERSION   "2.4.3"
+#define APP_VERSION   "2.4.4"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
-#define APP_COPYRIGHT "Copyright (C) 2016-2017 xmrig.com"
+#define APP_COPYRIGHT "Copyright (C) 2016-2018 xmrig.com"
 #define APP_KIND      "cpu"
 
 #define APP_VER_MAJOR  2
@@ -39,7 +39,7 @@
 #define APP_VER_REV    0
 
 #ifdef _MSC_VER
-#   if (_MSC_VER == 1910 || _MSC_VER == 1911)
+#   if (_MSC_VER >= 1910)
 #       define MSVC_VERSION 2017
 #   elif _MSC_VER == 1900
 #       define MSVC_VERSION 2015
