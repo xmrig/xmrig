ba3a62ca884f546e061a467a5a0a96ed222cc4dc
BenDroid <ben@graef.in>
2018-01-20 13:46:46 +0100

    Fixed arm build and windows build server script

--- src/Cpu_arm.cpp
+++ src/Cpu_arm.cpp
@@ -26,13 +26,14 @@
 
 
 #include "Cpu.h"
+#include "CpuImpl.h"
 
 void CpuImpl::initCommon()
 {
     memcpy(m_brand, "Unknown", 7);
 
 #   if defined(XMRIG_ARMv8)
-    m_flags |= X86_64;
-    m_flags |= AES;
+    m_flags |= Cpu::X86_64;
+    m_flags |= Cpu::AES;
 #   endif
 }
