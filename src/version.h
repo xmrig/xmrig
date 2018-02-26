27700182ff32e7336e70f71cdd8753f7ddc33f96
enWILLYado <enwillyado@hotmail.com>
2018-02-26 21:38:44 +0100

    New version!
    3.0.0 - 1 (eWa)

--- src/version.h
+++ src/version.h
@@ -24,19 +24,19 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-#define APP_ID        "xmrig"
-#define APP_NAME      "XMRig"
+#define APP_ID        "xmrig-ewa"
+#define APP_NAME      "XMRig-ewa"
 #define APP_DESC      "XMRig CPU miner"
 #define APP_VERSION   "2.4.4"
-#define APP_DOMAIN    "xmrig.com"
-#define APP_SITE      "www.xmrig.com"
-#define APP_COPYRIGHT "Copyright (C) 2016-2018 xmrig.com"
+#define APP_DOMAIN    "enWILLYado.com"
+#define APP_SITE      "www.enWILLYado.com"
+#define APP_COPYRIGHT "Copyright (C) 2016-2018 xmrig.com & enWILLYado.com"
 #define APP_KIND      "cpu"
 
-#define APP_VER_MAJOR  2
-#define APP_VER_MINOR  4
-#define APP_VER_BUILD  4
-#define APP_VER_REV    0
+#define APP_VER_MAJOR  3
+#define APP_VER_MINOR  0
+#define APP_VER_BUILD  0
+#define APP_VER_REV    1
 
 #ifdef _MSC_VER
 #   if (_MSC_VER >= 1910)
