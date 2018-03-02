85ee140ea77818a3843c4f0b2454a2eccb29f72a
BenDroid <ben@graef.in>
2018-03-02 21:54:23 +0100

    Fixed merge issues

--- src/crypto/CryptoNight_x86.h
+++ src/crypto/CryptoNight_x86.h
@@ -353,12 +353,11 @@ public:
 
         for (size_t i = 0; i < ITERATIONS; i++) {
             for (size_t hashBlock = 0; hashBlock < NUM_HASH_BLOCKS; ++hashBlock) {
-                __m128i cx;
+                __m128i cx = _mm_load_si128((__m128i*) &l[hashBlock][idx[hashBlock] & MASK]);
 
                 if (SOFT_AES) {
                     cx = soft_aesenc(cx, _mm_set_epi64x(ah[hashBlock], al[hashBlock]));
                 } else {
-                    cx = _mm_load_si128((__m128i*) &l[hashBlock][idx[hashBlock] & MASK]);
                     cx = _mm_aesenc_si128(cx, _mm_set_epi64x(ah[hashBlock], al[hashBlock]));
                 }
 
@@ -425,9 +424,9 @@ public:
             __m128i cx;
 
             if (SOFT_AES) {
-                cx = soft_aesenc(cx, _mm_set_epi64x(ah, al));
+                cx = soft_aesenc((uint32_t*)&l[idx & MASK], _mm_set_epi64x(ah, al));
             } else {
-                cx = _mm_load_si128((__m128i*) &l[hashBlock][idx[hashBlock] & MASK]);
+                cx = _mm_load_si128((__m128i*) &l[idx & MASK]);
                 cx = _mm_aesenc_si128(cx, _mm_set_epi64x(ah, al));
             }
 
@@ -493,11 +492,12 @@ public:
             __m128i cx1;
 
             if (SOFT_AES) {
-                cx0 = soft_aesenc(cx0, _mm_set_epi64x(ah0, al0));
-                cx1 = soft_aesenc(cx1, _mm_set_epi64x(ah1, al1));
+                cx0 = soft_aesenc((uint32_t*)&l0[idx0 & MASK], _mm_set_epi64x(ah0, al0));
+                cx1 = soft_aesenc((uint32_t*)&l1[idx1 & MASK], _mm_set_epi64x(ah1, al1));
             } else {
                 cx0 = _mm_load_si128((__m128i*) &l0[idx0 & MASK]);
                 cx1 = _mm_load_si128((__m128i*) &l1[idx1 & MASK]);
+
                 cx0 = _mm_aesenc_si128(cx0, _mm_set_epi64x(ah0, al0));
                 cx1 = _mm_aesenc_si128(cx1, _mm_set_epi64x(ah1, al1));
             }
@@ -597,13 +597,14 @@ public:
             __m128i cx2;
 
             if (SOFT_AES) {
-                cx0 = soft_aesenc(cx0, _mm_set_epi64x(ah0, al0));
-                cx1 = soft_aesenc(cx1, _mm_set_epi64x(ah1, al1));
-                cx2 = soft_aesenc(cx2, _mm_set_epi64x(ah2, al2));
+                cx0 = soft_aesenc((uint32_t*)&l0[idx0 & MASK], _mm_set_epi64x(ah0, al0));
+                cx1 = soft_aesenc((uint32_t*)&l1[idx1 & MASK], _mm_set_epi64x(ah1, al1));
+                cx2 = soft_aesenc((uint32_t*)&l2[idx2 & MASK], _mm_set_epi64x(ah2, al2));
             } else {
                 cx0 = _mm_load_si128((__m128i*) &l0[idx0 & MASK]);
                 cx1 = _mm_load_si128((__m128i*) &l1[idx1 & MASK]);
                 cx2 = _mm_load_si128((__m128i*) &l2[idx2 & MASK]);
+
                 cx0 = _mm_aesenc_si128(cx0, _mm_set_epi64x(ah0, al0));
                 cx1 = _mm_aesenc_si128(cx1, _mm_set_epi64x(ah1, al1));
                 cx2 = _mm_aesenc_si128(cx2, _mm_set_epi64x(ah2, al2));
@@ -736,10 +737,10 @@ public:
             __m128i cx3;
 
             if (SOFT_AES) {
-                cx0 = soft_aesenc(cx0, _mm_set_epi64x(ah0, al0));
-                cx1 = soft_aesenc(cx1, _mm_set_epi64x(ah1, al1));
-                cx2 = soft_aesenc(cx2, _mm_set_epi64x(ah2, al2));
-                cx3 = soft_aesenc(cx3, _mm_set_epi64x(ah3, al3));
+                cx0 = soft_aesenc((uint32_t*)&l0[idx0 & MASK], _mm_set_epi64x(ah0, al0));
+                cx1 = soft_aesenc((uint32_t*)&l1[idx1 & MASK], _mm_set_epi64x(ah1, al1));
+                cx2 = soft_aesenc((uint32_t*)&l2[idx2 & MASK], _mm_set_epi64x(ah2, al2));
+                cx3 = soft_aesenc((uint32_t*)&l3[idx3 & MASK], _mm_set_epi64x(ah3, al3));
             } else {
                 cx0 = _mm_load_si128((__m128i*) &l0[idx0 & MASK]);
                 cx1 = _mm_load_si128((__m128i*) &l1[idx1 & MASK]);
@@ -909,11 +910,11 @@ public:
             __m128i cx4;
 
             if (SOFT_AES) {
-                cx0 = soft_aesenc(cx0, _mm_set_epi64x(ah0, al0));
-                cx1 = soft_aesenc(cx1, _mm_set_epi64x(ah1, al1));
-                cx2 = soft_aesenc(cx2, _mm_set_epi64x(ah2, al2));
-                cx3 = soft_aesenc(cx3, _mm_set_epi64x(ah3, al3));
-                cx4 = soft_aesenc(cx4, _mm_set_epi64x(ah4, al4));
+                cx0 = soft_aesenc((uint32_t*)&l0[idx0 & MASK], _mm_set_epi64x(ah0, al0));
+                cx1 = soft_aesenc((uint32_t*)&l1[idx1 & MASK], _mm_set_epi64x(ah1, al1));
+                cx2 = soft_aesenc((uint32_t*)&l2[idx2 & MASK], _mm_set_epi64x(ah2, al2));
+                cx3 = soft_aesenc((uint32_t*)&l3[idx3 & MASK], _mm_set_epi64x(ah3, al3));
+                cx4 = soft_aesenc((uint32_t*)&l4[idx4 & MASK], _mm_set_epi64x(ah4, al4));
             } else {
                 cx0 = _mm_load_si128((__m128i*) &l0[idx0 & MASK]);
                 cx1 = _mm_load_si128((__m128i*) &l1[idx1 & MASK]);
