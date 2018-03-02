b799a28923ce4aaa46b9dbde2799a9bb80bb0ed0
BenDroid <ben@graef.in>
2018-03-02 20:51:48 +0100

    Fixed merging

--- src/cc/Httpd.cpp
+++ src/cc/Httpd.cpp
@@ -58,7 +58,7 @@ bool Httpd::start()
 
         m_daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY | MHD_USE_SSL,
                                    static_cast<uint16_t>(m_options->ccPort()), nullptr, nullptr, &Httpd::handler,
-                                   this, MHD_OPTION_CONNECTION_TIMEOUT, (unsigned int) 10,
+                                   this, MHD_OPTION_CONNECTION_TIMEOUT, (unsigned int) 25,
                                    MHD_OPTION_HTTPS_MEM_KEY, m_keyPem.c_str(),
                                    MHD_OPTION_HTTPS_MEM_CERT, m_certPem.c_str(),
                                    MHD_OPTION_END);
@@ -66,7 +66,7 @@ bool Httpd::start()
 #   endif
         m_daemon = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY, static_cast<uint16_t>(m_options->ccPort()), nullptr,
                                     nullptr, &Httpd::handler,
-                                    this, MHD_OPTION_CONNECTION_TIMEOUT, (unsigned int) 10, MHD_OPTION_END);
+                                    this, MHD_OPTION_CONNECTION_TIMEOUT, (unsigned int) 25, MHD_OPTION_END);
 #   ifndef XMRIG_NO_TLS
     }
 #   endif
