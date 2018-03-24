1011fd48916446ac4d03f619760c5b7615139164
XMRig <support@xmrig.com>
2018-03-24 10:14:21 +0700

    Partial fix for ARMv7

--- src/crypto/CryptoNight_monero.h
+++ src/crypto/CryptoNight_monero.h
@@ -27,12 +27,22 @@
 
 
 // VARIANT ALTERATIONS
-#define VARIANT1_INIT(part) \
+#ifndef XMRIG_ARM
+#   define VARIANT1_INIT(part) \
     uint64_t tweak1_2_##part = 0; \
     if (VARIANT > 0) { \
         tweak1_2_##part = (*reinterpret_cast<const uint64_t*>(input + 35 + part * size) ^ \
                           *(reinterpret_cast<const uint64_t*>(ctx->state##part) + 24)); \
     }
+#else
+#   define VARIANT1_INIT(part) \
+    uint64_t tweak1_2_##part = 0; \
+    if (VARIANT > 0) { \
+        volatile const uint64_t a = *reinterpret_cast<const uint64_t*>(input + 35 + part * size); \
+        volatile const uint64_t b = *(reinterpret_cast<const uint64_t*>(ctx->state##part) + 24); \
+        tweak1_2_##part = a ^ b; \
+    }
+#endif
 
 #define VARIANT1_1(p) \
     if (VARIANT > 0) { \
