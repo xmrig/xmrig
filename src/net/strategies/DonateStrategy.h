d0ff7dc83018767a55c4a664d57fe36a5b5248d3
BenDroid <ben@graef.in>
2018-03-25 14:41:56 +0200

    Increased version and changed donationstrategy again

--- src/net/strategies/DonateStrategy.h
+++ src/net/strategies/DonateStrategy.h
@@ -58,7 +58,7 @@ protected:
     void onResultAccepted(Client *client, const SubmitResult &result, const char *error) override;
 
 private:
-    void idle(int timeout);
+    void idle(uint64_t timeout);
     void suspend();
 
     static void onTimer(uv_timer_t *handle);
