44d56393dbb14bee4e7470de07f9bf3e3c0bf324
XMRig <support@xmrig.com>
2018-03-31 20:26:07 +0700

    Fix Linux build.

--- src/core/Config.cpp
+++ src/core/Config.cpp
@@ -139,7 +139,7 @@ void xmrig::Config::getJSON(rapidjson::Document &doc) const
     doc.AddMember("user-agent",    userAgent() ? rapidjson::Value(rapidjson::StringRef(userAgent())).Move() : rapidjson::Value(rapidjson::kNullType).Move(), allocator);
 
 #   ifdef HAVE_SYSLOG_H
-    doc.AddMember("syslog", syslog(), allocator);
+    doc.AddMember("syslog", isSyslog(), allocator);
 #   endif
 
     doc.AddMember("watch", m_watch, allocator);
