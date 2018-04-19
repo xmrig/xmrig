581cf9794d1eed861a7f842bc611481157f0f87f
Ben Gräf <ben@gra3f.de>
2018-04-19 16:05:33 +0200

    Update Job.h

--- src/net/Job.h
+++ src/net/Job.h
@@ -68,7 +68,7 @@ public:
     bool operator!=(const Job &other) const;
 
 private:
-    alignas(16) uint8_t m_blob[84]; // Max blob size is 84 (75 fixed + 9 variable), aligned to 96. https://github.com/xmrig/xmrig/issues/1 Thanks fireice-uk.
+    uint8_t m_blob[96]; // Max blob size is 84 (75 fixed + 9 variable), aligned to 96. https://github.com/xmrig/xmrig/issues/1 Thanks fireice-uk.
 
     bool m_nicehash;
     int m_poolId;
@@ -78,11 +78,6 @@ private:
     uint64_t m_diff;
     uint64_t m_target;
     Options::PowVersion m_powVersion;
-
-#   ifdef XMRIG_PROXY_PROJECT
-    VAR_ALIGN(16, char m_rawBlob[169]);
-    VAR_ALIGN(16, char m_rawTarget[17]);
-#   endif
 };
 
 #endif /* __JOB_H__ */
