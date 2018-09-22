30791de5bc41baee1efea45900c9e778d009ac06
XMRig <support@xmrig.com>
2018-09-22 16:01:27 +0300

    Don't use static OpenSSL on Linux by default.

--- cmake/OpenSSL.cmake
+++ cmake/OpenSSL.cmake
@@ -1,7 +1,12 @@
 if (WITH_TLS)
     set(OPENSSL_ROOT_DIR ${XMRIG_DEPS})
-    set(OPENSSL_USE_STATIC_LIBS TRUE)
-    set(OPENSSL_MSVC_STATIC_RT TRUE)
+
+    if (WIN32)
+        set(OPENSSL_USE_STATIC_LIBS TRUE)
+        set(OPENSSL_MSVC_STATIC_RT TRUE)
+
+        set(EXTRA_LIBS ${EXTRA_LIBS} Crypt32)
+    endif()
 
     find_package(OpenSSL)
 
@@ -10,10 +15,6 @@ if (WITH_TLS)
         include_directories(${OPENSSL_INCLUDE_DIR})
     else()
         message(FATAL_ERROR "OpenSSL NOT found: use `-DWITH_TLS=OFF` to build without TLS support")
-    endif()
-
-    if (WIN32)
-        set(EXTRA_LIBS ${EXTRA_LIBS} Crypt32)
     endif()
 else()
     set(TLS_SOURCES "")
