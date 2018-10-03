2ea911063d87c5a1fd27cedcd8684891594cb18c
MoneroOcean <support@moneroocean.stream>
2018-10-04 00:02:41 +0200

    Removed not needed stuff

--- build_rh6.sh
+++ build_rh6.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-yum update -y &&\
-yum install -y cmake make git openssl-devel libmicrohttpd-devel &&\
-rpm -i https://github.com/sipcapture/captagent/raw/master/dependency/centos/6/libuv-1.8.0-1.el6.x86_64.rpm &&\
-rpm -i https://github.com/sipcapture/captagent/raw/master/dependency/centos/6/libuv-devel-1.8.0-1.el6.x86_64.rpm &&\
-wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo &&\
-yum upgrade -y &&\
-yum install -y devtoolset-2-gcc devtoolset-2-binutils devtoolset-2-gcc-c++ &&\
+yum update -y
+yum install -y cmake make git openssl-devel libmicrohttpd-devel
+rpm -i https://github.com/sipcapture/captagent/raw/master/dependency/centos/6/libuv-1.8.0-1.el6.x86_64.rpm
+rpm -i https://github.com/sipcapture/captagent/raw/master/dependency/centos/6/libuv-devel-1.8.0-1.el6.x86_64.rpm
+wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo
+yum upgrade -y
+yum install -y devtoolset-2-gcc devtoolset-2-binutils devtoolset-2-gcc-c++
+
 git checkout $1 &&\
 scl enable devtoolset-2 "cmake ." &&\
 scl enable devtoolset-2 "make" &&\
