d1e896b745faf86f15a4807e35ab69f6d49807bc
Ben Gräf <ben@gra3f.de>
2018-10-21 19:02:41 +0200

    Removed obsolete fork workarounds for old versions of xtl, msr, xhv

--- src/net/Job.cpp
+++ src/net/Job.cpp
@@ -146,15 +146,6 @@ PowVariant Job::powVariant() const
         } else {
             return PowVariant::POW_V0;
         }
-    }
-    else if (m_powVariant == PowVariant::POW_XTL && m_blob[0] < 4) {
-        return POW_V1;
-    }
-    else if (m_powVariant == PowVariant::POW_MSR && m_blob[0] < 7) {
-        return POW_V1;
-    }
-    else if (m_powVariant == PowVariant::POW_XHV && m_blob[0] < 3) {
-        return POW_V0;
     } else {
         return m_powVariant;
     }
