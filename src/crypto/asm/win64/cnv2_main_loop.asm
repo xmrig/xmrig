06a84499d7e5100686c4757e68df3223bc2ed716
XMRig <support@xmrig.com>
2018-10-22 23:08:29 +0700

    Fixed MSYS2 build & copyright

--- src/crypto/asm/win64/cnv2_main_loop.asm
+++ src/crypto/asm/win64/cnv2_main_loop.asm
@@ -1,6 +1,7 @@
 _TEXT_CNV2_MAINLOOP SEGMENT PAGE READ EXECUTE
 PUBLIC cnv2_mainloop_ivybridge_asm
 PUBLIC cnv2_mainloop_ryzen_asm
+PUBLIC cnv2_mainloop_bulldozer_asm
 PUBLIC cnv2_double_mainloop_sandybridge_asm
 
 ALIGN 64
@@ -14,6 +15,12 @@ cnv2_mainloop_ryzen_asm PROC
 	INCLUDE cnv2_main_loop_ryzen.inc
 	ret 0
 cnv2_mainloop_ryzen_asm ENDP
+
+ALIGN 64
+cnv2_mainloop_bulldozer_asm PROC
+	INCLUDE cnv2_main_loop_bulldozer.inc
+	ret 0
+cnv2_mainloop_bulldozer_asm ENDP
 
 ALIGN 64
 cnv2_double_mainloop_sandybridge_asm PROC
