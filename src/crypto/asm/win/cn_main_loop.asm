f545fa52cb928b915fa76de69320eb1c794487c5
Ben Gräf <ben@gra3f.de>
2019-01-04 19:45:16 +0100

    Fixed windows asm files

--- src/crypto/asm/win/cn_main_loop.asm
+++ src/crypto/asm/win/cn_main_loop.asm
@@ -6,17 +6,17 @@ PUBLIC cnv2_mainloop_ivybridge_asm
 PUBLIC cnv2_mainloop_ryzen_asm
 PUBLIC cnv2_mainloop_bulldozer_asm
 PUBLIC cnv2_double_mainloop_sandybridge_asm
-PUBLIC cn_fast2_mainloop_ivybridge_asm
-PUBLIC cn_fast2_mainloop_ryzen_asm
-PUBLIC cn_fast2_mainloop_bulldozer_asm
-PUBLIC cn_fast2_double_mainloop_sandybridge_asm
+PUBLIC cn_fastv2mainloop_ivybridge_asm
+PUBLIC cn_fastv2mainloop_ryzen_asm
+PUBLIC cn_fastv2mainloop_bulldozer_asm
+PUBLIC cn_fastv2double_mainloop_sandybridge_asm
 PUBLIC cn_liteupx_mainloop_sandybridge_asm
 
 PUBLIC cnv1_mainloop_soft_aes_sandybridge_asm
 PUBLIC cn_litev1_mainloop_soft_aes_sandybridge_asm
 PUBLIC cn_fast_mainloop_soft_aes_sandybridge_asm
 PUBLIC cnv2_mainloop_soft_aes_sandybridge_asm
-PUBLIC cn_fast2_mainloop_soft_aes_sandybridge_asm
+PUBLIC cn_fastv2mainloop_soft_aes_sandybridge_asm
 PUBLIC cn_liteupx_mainloop_soft_aes_sandybridge_asm
 
 ALIGN 64
@@ -62,28 +62,28 @@ cnv2_double_mainloop_sandybridge_asm PROC
 cnv2_double_mainloop_sandybridge_asm ENDP
 
 ALIGN 64
-cn_fast2_mainloop_ivybridge_asm PROC
-	INCLUDE cn_fast2_main_loop_ivybridge.inc
+cn_fastv2mainloop_ivybridge_asm PROC
+	INCLUDE cn_fastv2main_loop_ivybridge.inc
 	ret 0
-cn_fast2_mainloop_ivybridge_asm ENDP
+cn_fastv2mainloop_ivybridge_asm ENDP
 
 ALIGN 64
-cn_fast2_mainloop_ryzen_asm PROC
-	INCLUDE cn_fast2_main_loop_ryzen.inc
+cn_fastv2mainloop_ryzen_asm PROC
+	INCLUDE cn_fastv2main_loop_ryzen.inc
 	ret 0
-cn_fast2_mainloop_ryzen_asm ENDP
+cn_fastv2mainloop_ryzen_asm ENDP
 
 ALIGN 64
-cn_fast2_mainloop_bulldozer_asm PROC
-	INCLUDE cn_fast2_main_loop_bulldozer.inc
+cn_fastv2mainloop_bulldozer_asm PROC
+	INCLUDE cn_fastv2main_loop_bulldozer.inc
 	ret 0
-cn_fast2_mainloop_bulldozer_asm ENDP
+cn_fastv2mainloop_bulldozer_asm ENDP
 
 ALIGN 64
-cn_fast2_double_mainloop_sandybridge_asm PROC
-	INCLUDE cn_fast2_double_main_loop_sandybridge.inc
+cn_fastv2double_mainloop_sandybridge_asm PROC
+	INCLUDE cn_fastv2double_main_loop_sandybridge.inc
 	ret 0
-cn_fast2_double_mainloop_sandybridge_asm ENDP
+cn_fastv2double_mainloop_sandybridge_asm ENDP
 
 ALIGN 64
 cn_liteupx_mainloop_sandybridge_asm PROC
@@ -116,10 +116,10 @@ cnv2_mainloop_soft_aes_sandybridge_asm PROC
 cnv2_mainloop_soft_aes_sandybridge_asm ENDP
 
 ALIGN 64
-cn_fast2_mainloop_soft_aes_sandybridge_asm PROC
-	INCLUDE cn_fast2_mainloop_soft_aes_sandybridge.inc
+cn_fastv2mainloop_soft_aes_sandybridge_asm PROC
+	INCLUDE cn_fastv2mainloop_soft_aes_sandybridge.inc
 	ret 0
-cn_fast2_mainloop_soft_aes_sandybridge_asm ENDP
+cn_fastv2mainloop_soft_aes_sandybridge_asm ENDP
 
 _TEXT_CN_MAINLOOP ENDS
 END
