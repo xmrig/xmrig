189eda646793e585897141a6043ae37a3f8d5941
SChernykh <sergey.v.chernykh@gmail.com>
2019-01-16 00:09:00 +0100

    Fixed crash in patchCode() on Linux

--- src/crypto/asm/cn_main_loop.asm
+++ src/crypto/asm/cn_main_loop.asm
@@ -8,40 +8,28 @@ ALIGN(64)
 cnv2_mainloop_ivybridge_asm PROC
 	INCLUDE cn2/cnv2_main_loop_ivybridge.inc
 	ret 0
-	nop
-	nop
-	nop
-	nop
+	mov eax, 0xDEADC0DE
 cnv2_mainloop_ivybridge_asm ENDP
 
 ALIGN(64)
 cnv2_mainloop_ryzen_asm PROC
 	INCLUDE cn2/cnv2_main_loop_ryzen.inc
 	ret 0
-	nop
-	nop
-	nop
-	nop
+	mov eax, 0xDEADC0DE
 cnv2_mainloop_ryzen_asm ENDP
 
 ALIGN(64)
 cnv2_mainloop_bulldozer_asm PROC
 	INCLUDE cn2/cnv2_main_loop_bulldozer.inc
 	ret 0
-	nop
-	nop
-	nop
-	nop
+	mov eax, 0xDEADC0DE
 cnv2_mainloop_bulldozer_asm ENDP
 
 ALIGN(64)
 cnv2_double_mainloop_sandybridge_asm PROC
 	INCLUDE cn2/cnv2_double_main_loop_sandybridge.inc
 	ret 0
-	nop
-	nop
-	nop
-	nop
+	mov eax, 0xDEADC0DE
 cnv2_double_mainloop_sandybridge_asm ENDP
 
 _TEXT_CNV2_MAINLOOP ENDS
