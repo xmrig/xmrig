897ff83dcdc0f4320c5475f3734a015d01835cde
XMRig <support@xmrig.com>
2019-01-18 21:44:44 +0700

    #913 Increase max blob size for MSR secor upgrade.

--- src/workers/MultiWorker.h
+++ src/workers/MultiWorker.h
@@ -6,7 +6,7 @@
  * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
  * Copyright 2017-2018 XMR-Stak    <https://github.com/fireice-uk>, <https://github.com/psychocrypt>
  * Copyright 2018      Lee Clagett <https://github.com/vtnerd>
- * Copyright 2018      SChernykh   <https://github.com/SChernykh>
+ * Copyright 2018-2019 SChernykh   <https://github.com/SChernykh>
  * Copyright 2016-2019 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
  *
  *   This program is free software: you can redistribute it and/or modify
@@ -60,7 +60,7 @@ private:
 
     struct State
     {
-        alignas(16) uint8_t blob[96 * N];
+        alignas(16) uint8_t blob[Job::kMaxBlobSize * N];
         Job job;
     };
 
