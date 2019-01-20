9692f92c6b971cf90af7d59308646afd7b9027c2
XMRig <support@xmrig.com>
2019-01-21 00:37:36 +0700

    New default value for max-cpu-usage option is 100 (instead of 75) also this option now deprecated.

--- src/core/Config.cpp
+++ src/core/Config.cpp
@@ -5,7 +5,8 @@
  * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
  * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
  * Copyright 2017-2018 XMR-Stak    <https://github.com/fireice-uk>, <https://github.com/psychocrypt>
- * Copyright 2016-2018 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ * Copyright 2018-2019 SChernykh   <https://github.com/SChernykh>
+ * Copyright 2016-2019 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -48,7 +49,7 @@ xmrig::Config::Config() : xmrig::CommonConfig(),
     m_hugePages(true),
     m_safe(false),
     m_shouldSave(false),
-    m_maxCpuUsage(75),
+    m_maxCpuUsage(100),
     m_priority(-1)
 {
 }
