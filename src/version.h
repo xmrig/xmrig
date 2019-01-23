2c0183dcef18efd7563a96707609acfb61fd0f08
XMRig <support@xmrig.com>
2019-01-23 19:31:45 +0700

    v2.10.0

--- src/version.h
+++ src/version.h
@@ -28,15 +28,15 @@
 #define APP_ID        "xmrig"
 #define APP_NAME      "XMRig"
 #define APP_DESC      "XMRig CPU miner"
-#define APP_VERSION   "2.9.5-dev"
+#define APP_VERSION   "2.10.0"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
 #define APP_COPYRIGHT "Copyright (C) 2016-2019 xmrig.com"
 #define APP_KIND      "cpu"
 
 #define APP_VER_MAJOR  2
-#define APP_VER_MINOR  9
-#define APP_VER_PATCH  5
+#define APP_VER_MINOR  10
+#define APP_VER_PATCH  0
 
 #ifdef _MSC_VER
 #   if (_MSC_VER >= 1910)
