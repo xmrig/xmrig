5ae25845fa850dba88e0d5d5ffe47b8ded956778
MoneroOcean <support@moneroocean.stream>
2019-02-21 21:37:53 -0800

    Fixed install

--- build_rh6.sh
+++ build_rh6.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 yum update -y
-yum install -y cmake make git openssl-devel libmicrohttpd-devel
+yum upgrade -y
+yum install -y cmake make git openssl-devel libmicrohttpd-devel centos-release-scl-rh
+yum install -y --nogpgcheck devtoolset-6-gcc devtoolset-6-binutils devtoolset-6-gcc-c++
 rpm -i https://github.com/sipcapture/captagent/raw/master/dependency/centos/6/libuv-1.8.0-1.el6.x86_64.rpm
 rpm -i https://github.com/sipcapture/captagent/raw/master/dependency/centos/6/libuv-devel-1.8.0-1.el6.x86_64.rpm
-wget http://linuxsoft.cern.ch/cern/scl/slc6-scl.repo -O /etc/yum.repos.d/slc6-scl.repo
-yum upgrade -y
-yum install -y --nogpgcheck devtoolset-6-gcc devtoolset-6-binutils devtoolset-6-gcc-c++
-
 rm -rf build xmrig-$1-lin64.tar.gz
 mkdir build &&\
 cd build &&\
