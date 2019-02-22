6dd971898352f9c0e17b8858e3502a121d8ef858
MoneroOcean <support@moneroocean.stream>
2019-02-21 17:19:22 -0800

    Fixed RH6 build

--- build_rh6.sh
+++ build_rh6.sh
@@ -3,9 +3,9 @@ yum update -y
 yum install -y cmake make git openssl-devel libmicrohttpd-devel
 rpm -i https://github.com/sipcapture/captagent/raw/master/dependency/centos/6/libuv-1.8.0-1.el6.x86_64.rpm
 rpm -i https://github.com/sipcapture/captagent/raw/master/dependency/centos/6/libuv-devel-1.8.0-1.el6.x86_64.rpm
-wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo
+wget http://linuxsoft.cern.ch/cern/scl/slc6-scl.repo -O /etc/yum.repos.d/slc6-scl.repo
 yum upgrade -y
-yum install -y devtoolset-2-gcc devtoolset-2-binutils devtoolset-2-gcc-c++
+yum install -y devtoolset-6-gcc devtoolset-6-binutils devtoolset-6-gcc-c++
 
 rm -rf build xmrig-$1-lin64.tar.gz
 mkdir build &&\
@@ -13,8 +13,8 @@ cd build &&\
 git clone https://github.com/MoneroOcean/xmrig.git &&\
 cd xmrig &&\
 git checkout $1 &&\
-scl enable devtoolset-2 "cmake . -DWITH_TLS=OFF -DWITH_HTTPD=OFF" &&\
-scl enable devtoolset-2 "make" &&\
+scl enable devtoolset-6 "cmake . -DWITH_TLS=OFF -DWITH_HTTPD=OFF" &&\
+scl enable devtoolset-6 "make" &&\
 cp src/config.json . &&\
 mv xmrig-notls xmrig &&\
 tar cfz ../../xmrig-$1-lin64.tar.gz xmrig config.json &&\
