d117c3b797e6d1ddb01463db9726d5fd2c0a600d
MoneroOcean <support@moneroocean.stream>
2019-03-07 09:13:19 -0800

    Improved command flow

--- build_rh6.sh
+++ build_rh6.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 yum update -y
 yum upgrade -y
-yum install -y cmake make git openssl-devel libmicrohttpd-devel centos-release-scl-rh
+yum install -y git
+yum install -y cmake make openssl-devel libmicrohttpd-devel centos-release-scl-rh
 yum install -y --nogpgcheck devtoolset-6-gcc devtoolset-6-binutils devtoolset-6-gcc-c++
 rpm -i https://github.com/sipcapture/captagent/raw/master/dependency/centos/6/libuv-1.8.0-1.el6.x86_64.rpm
 rpm -i https://github.com/sipcapture/captagent/raw/master/dependency/centos/6/libuv-devel-1.8.0-1.el6.x86_64.rpm
