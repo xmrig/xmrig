ca70cd4d7551bb15bd1b48e53563b46092fc0d07
MoneroOcean <support@moneroocean.stream>
2019-06-18 17:18:59 -0700

    Fixed 7z name

--- build.bat
+++ build.bat
@@ -13,6 +13,6 @@ cmake .. -G "Visual Studio 15 2017 Win64" -DXMRIG_DEPS=%~dp0\build\xmrig-deps\ms
 msbuild /p:Configuration=Release xmrig.sln &&^
 cd Release &&^
 copy ..\..\src\config.json . &&^
-7za a -tzip -mx %~dp0\xmrig-%1-win64.zip xmrig.exe config.json &&^
+7z a -tzip -mx %~dp0\xmrig-%1-win64.zip xmrig.exe config.json &&^
 cd %~dp0 &&^
 rmdir /S /Q build
