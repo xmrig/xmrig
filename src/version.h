b68132f88416803954ec9555de7421e3e7fc0ca5
MoneroOcean <support@moneroocean.stream>
2019-06-26 09:03:04 -0700

    Fixed Windows RandomX support

--- src/version.h
+++ src/version.h
@@ -28,7 +28,7 @@
 #define APP_ID        "xmrig"
 #define APP_NAME      "XMRig"
 #define APP_DESC      "XMRig CPU miner"
-#define APP_VERSION   "2.15.4-beta-mo2"
+#define APP_VERSION   "2.15.4-beta-mo3"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
 #define APP_COPYRIGHT "Copyright (C) 2016-2019 xmrig.com"
