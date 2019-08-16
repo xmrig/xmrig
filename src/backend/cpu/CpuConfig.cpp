f315063b94be60f89f6f09ab6376bc59472ecc07
MoneroOcean <support@moneroocean.stream>
2019-08-15 18:47:12 -0700

    Added DefyX profile

--- src/backend/cpu/CpuConfig.cpp
+++ src/backend/cpu/CpuConfig.cpp
@@ -173,7 +173,6 @@ void xmrig::CpuConfig::generate()
 #   ifdef XMRIG_ALGO_RANDOMX
     m_threads.move(kRx, cpu->threads(Algorithm::RX_0));
     m_threads.move(kRxWOW, cpu->threads(Algorithm::RX_WOW));
-    m_threads.move("defyx", cpu->threads(Algorithm::DEFYX));
 #   endif
 }
 
