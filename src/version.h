91462b4409a646fca2cfbe6133d2e1cd334eb379
MoneroOcean <support@moneroocean.stream>
2019-10-30 08:41:53 -0700

    Moved version

--- src/version.h
+++ src/version.h
@@ -28,7 +28,7 @@
 #define APP_ID        "xmrig"
 #define APP_NAME      "XMRig"
 #define APP_DESC      "XMRig miner"
-#define APP_VERSION   "4.4.0-beta-mo1"
+#define APP_VERSION   "4.4.0-beta-mo2"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
 #define APP_COPYRIGHT "Copyright (C) 2016-2019 xmrig.com"
