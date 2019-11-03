a6b46e4c58a3c37afdd46b64e972387030b2034c
MoneroOcean <support@moneroocean.stream>
2019-11-03 08:58:15 -0800

    Fixed OCL benchmark issues

--- src/core/Benchmark.cpp
+++ src/core/Benchmark.cpp
@@ -141,7 +141,7 @@ void Benchmark::start(const BenchAlgo bench_algo) {
     const Algorithm algo(ba2a[bench_algo]);
     for (auto backend : m_controller->miner()->backends()) if (backend->isEnabled() && backend->isEnabled(algo)) ++ m_enabled_backend_count;
     if (m_enabled_backend_count == 0) {
-        run_next_bench_algo(m_bench_algo);
+        run_next_bench_algo(bench_algo);
         return;
     }
     // prepare test job for benchmark runs ("benchmark" client id is to make sure we can detect benchmark jobs)
@@ -150,6 +150,7 @@ void Benchmark::start(const BenchAlgo bench_algo) {
     // 99 here to trigger all future bench_algo versions for auto veriant detection based on block version
     job.setBlob("9905A0DBD6BF05CF16E503F3A66F78007CBF34144332ECBFC22ED95C8700383B309ACE1923A0964B00000008BA939A62724C0D7581FCE5761E9D8A0E6A1C3F924FDD8493D1115649C05EB601");
     job.setTarget("FFFFFFFFFFFFFF20"); // set difficulty to 8 cause onJobResult after every 8-th computed hash
+    job.setTarget(1000);
     job.setSeedHash("0000000000000000000000000000000000000000000000000000000000000001");
     m_bench_algo  = bench_algo; // current perf bench_algo
     m_hash_count  = 0;          // number of hashes calculated for current perf bench_algo
