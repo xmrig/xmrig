fb28b931cc2ad4fdae2ab19f3a1e3f8bf5675da2
XMRig <support@xmrig.com>
2019-11-16 18:56:34 +0700

    Use static OpenSSL libs on macOS.

--- cmake/OpenSSL.cmake
+++ cmake/OpenSSL.cmake
@@ -6,6 +6,8 @@ if (WITH_TLS)
         set(OPENSSL_MSVC_STATIC_RT TRUE)
 
         set(EXTRA_LIBS ${EXTRA_LIBS} Crypt32)
+    elseif (APPLE)
+        set(OPENSSL_USE_STATIC_LIBS TRUE)
     endif()
 
     find_package(OpenSSL)
