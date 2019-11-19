8102526abc6a9f99473c83c5e74fd86fcb8815a8
XMRig <support@xmrig.com>
2019-11-19 22:21:24 +0700

    Added build_deps.sh for creating xmrig-deps on Linux.

---
+++ scripts/build_deps.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+
+UV_VERSION="1.33.1"
+OPENSSL_VERSION="1.1.1d"
+HWLOC_VERSION="2.1.0"
+
+mkdir deps
+mkdir deps/include
+mkdir deps/lib
+
+mkdir build && cd build
+
+wget https://github.com/libuv/libuv/archive/v${UV_VERSION}.tar.gz
+tar -xzf v${UV_VERSION}.tar.gz
+
+wget https://download.open-mpi.org/release/hwloc/v2.1/hwloc-${HWLOC_VERSION}.tar.bz2
+tar -xjf hwloc-${HWLOC_VERSION}.tar.bz2
+
+wget https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
+tar -xzf openssl-${OPENSSL_VERSION}.tar.gz
+
+cd libuv-${UV_VERSION}
+sh autogen.sh
+./configure --disable-shared
+make -j$(nproc)
+cp -fr include/ ../../deps
+cp .libs/libuv.a ../../deps/lib
+cd ..
+
+cd hwloc-${HWLOC_VERSION}
+./configure --disable-shared --enable-static --disable-io --disable-libudev --disable-libxml2
+make -j$(nproc)
+cp -fr include/ ../../deps
+cp hwloc/.libs/libhwloc.a ../../deps/lib
+cd ..
+
+cd openssl-${OPENSSL_VERSION}
+./config -no-shared -no-asm -no-zlib -no-comp -no-dgram -no-filenames -no-cms
+make -j$(nproc)
+cp -fr include/ ../../deps
+cp libcrypto.a ../../deps/lib
+cp libssl.a ../../deps/lib
+cd ../..
