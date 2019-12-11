06e105821aa97b5d425066b41ff57f34d160560b
XMRig <support@xmrig.com>
2019-12-11 14:06:53 +0700

    v5.2.0

--- src/version.h
+++ src/version.h
@@ -28,7 +28,7 @@
 #define APP_ID        "xmrig"
 #define APP_NAME      "XMRig"
 #define APP_DESC      "XMRig miner"
-#define APP_VERSION   "5.2.0-dev"
+#define APP_VERSION   "5.2.0"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
 #define APP_COPYRIGHT "Copyright (C) 2016-2019 xmrig.com"
