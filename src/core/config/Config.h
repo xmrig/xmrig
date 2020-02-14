5e444553b113091e25ebac1f0edd6fad4604406c
XMRig <support@xmrig.com>
2020-02-15 04:07:11 +0700

    Fixed build without NVML.

--- src/core/config/Config.h
+++ src/core/config/Config.h
@@ -5,8 +5,8 @@
  * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
  * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
  * Copyright 2017-2018 XMR-Stak    <https://github.com/fireice-uk>, <https://github.com/psychocrypt>
- * Copyright 2018-2019 SChernykh   <https://github.com/SChernykh>
- * Copyright 2016-2019 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ * Copyright 2018-2020 SChernykh   <https://github.com/SChernykh>
+ * Copyright 2016-2020 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -67,8 +67,10 @@ public:
     const RxConfig &rx() const;
 #   endif
 
-#   if defined(XMRIG_FEATURE_NVML)
+#   if defined(XMRIG_FEATURE_NVML) || defined (XMRIG_FEATURE_ADL)
     uint32_t healthPrintTime() const;
+#   else
+    uint32_t healthPrintTime() const { return 0; }
 #   endif
 
     bool isShouldSave() const;
