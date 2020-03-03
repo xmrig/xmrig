5fa6a034d533dbf06fba2084018f7a0a7aa18108
XMRig <support@xmrig.com>
2020-03-04 00:06:20 +0700

    Formatting.

--- src/base/net/stratum/DaemonClient.cpp
+++ src/base/net/stratum/DaemonClient.cpp
@@ -271,8 +271,9 @@ bool xmrig::DaemonClient::parseJob(const rapidjson::Value &params, int *code)
 
     if (m_apiVersion == API_DERO) {
         // Truncate to 32 bytes to have the same data as in get_info RPC
-        if (m_prevHash.size() > 64)
+        if (m_prevHash.size() > 64) {
             m_prevHash.data()[64] = '\0';
+        }
     }
 
     if (m_state == ConnectingState) {
