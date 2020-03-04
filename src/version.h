c80ef54b60a7a832009f70e47ee14b886c60d19e
XMRig <support@xmrig.com>
2020-03-04 12:31:27 +0700

    v5.8.2-dev

--- src/version.h
+++ src/version.h
@@ -28,7 +28,7 @@
 #define APP_ID        "xmrig"
 #define APP_NAME      "XMRig"
 #define APP_DESC      "XMRig miner"
-#define APP_VERSION   "5.8.1"
+#define APP_VERSION   "5.8.2-dev"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
 #define APP_COPYRIGHT "Copyright (C) 2016-2020 xmrig.com"
@@ -36,7 +36,7 @@
 
 #define APP_VER_MAJOR  5
 #define APP_VER_MINOR  8
-#define APP_VER_PATCH  1
+#define APP_VER_PATCH  2
 
 #ifdef _MSC_VER
 #   if (_MSC_VER >= 1920)
