55292d7211ed929284af3d3571a16de4804aa99c
Jethro Grassie <jtg@xtrabass.com>
2020-08-21 14:48:05 -0400

    scripts: fix to pages per node, not processor

--- scripts/enable_1gb_pages.sh
+++ scripts/enable_1gb_pages.sh
@@ -2,7 +2,7 @@
 
 # https://xmrig.com/docs/miner/hugepages#onegb-huge-pages
 
-sysctl -w vm.nr_hugepages=$(nproc)
+sysctl -w vm.nr_hugepages=$(awk -F '-' '{print NF*3}' /sys/devices/system/node/online)
 
 for i in $(find /sys/devices/system/node/node* -maxdepth 0 -type d);
 do
