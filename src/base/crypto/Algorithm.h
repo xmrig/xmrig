7f2771b4660a09678ae7a2d350ee4f7916d7e2ca
Chris <chriskobasiuk@gmail.com>
2021-08-27 10:31:36 -0600

    Fixed Algorithm id
    Algorithm id should be 0x72151267, second and third byte encode L3 and L2 size.
    0x72 = 'r'
    0x15 = 1 << 0x15 (L3 size)
    0x12 = 1 << 0x12 (L2 size)
    0x67 = 'g'

--- src/base/crypto/Algorithm.h
+++ src/base/crypto/Algorithm.h
@@ -68,7 +68,7 @@ public:
         RX_0            = 0x72151200,   // "rx/0"             RandomX (reference configuration).
         RX_WOW          = 0x72141177,   // "rx/wow"           RandomWOW (Wownero).
         RX_ARQ          = 0x72121061,   // "rx/arq"           RandomARQ (Arqma).
-        RX_GRAFT        = 0x7257ef81,   // "rx/graft"         RandomGRAFT (Graft).
+        RX_GRAFT        = 0x72151267,   // "rx/graft"         RandomGRAFT (Graft).
         RX_SFX          = 0x72151273,   // "rx/sfx"           RandomSFX (Safex Cash).
         RX_KEVA         = 0x7214116b,   // "rx/keva"          RandomKEVA (Keva).
         AR2_CHUKWA      = 0x61130000,   // "argon2/chukwa"    Argon2id (Chukwa).
