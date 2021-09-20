7f4d667351483d484be34a0bd2ac2acc0d25c3e5
XMRig <support@xmrig.com>
2021-09-20 20:53:36 +0700

    Remove unnecessary string.

--- src/core/config/ConfigTransform.cpp
+++ src/core/config/ConfigTransform.cpp
@@ -17,6 +17,7 @@
  */
 
 #include "core/config/ConfigTransform.h"
+#include "base/crypto/Algorithm.h"
 #include "base/kernel/interfaces/IConfig.h"
 #include "base/net/stratum/Pool.h"
 #include "base/net/stratum/Pools.h"
@@ -43,7 +44,6 @@ static const char *kAsterisk    = "*";
 static const char *kEnabled     = "enabled";
 static const char *kIntensity   = "intensity";
 static const char *kThreads     = "threads";
-static const char *kKawPow      = "kawpow";
 
 
 static inline uint64_t intensity(uint64_t av)
@@ -103,7 +103,7 @@ void xmrig::ConfigTransform::finalize(rapidjson::Document &doc)
         profile.AddMember(StringRef(kThreads),   m_threads, allocator);
         profile.AddMember(StringRef(kAffinity),  m_affinity, allocator);
 
-        doc[CpuConfig::kField].AddMember(StringRef(kKawPow), false, doc.GetAllocator());
+        doc[CpuConfig::kField].AddMember(StringRef(Algorithm::kKAWPOW), false, doc.GetAllocator());
         doc[CpuConfig::kField].AddMember(StringRef(kAsterisk), profile, doc.GetAllocator());
     }
 
