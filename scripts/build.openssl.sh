6e39eee85be56795e13e9872e2919e3dd3813bae
MoneroOcean <support@moneroocean.stream>
2021-10-05 18:28:55 +0000

    Merge xmrig v6.15.2 into master

--- scripts/build.openssl.sh
+++ scripts/build.openssl.sh
@@ -8,7 +8,7 @@ mkdir -p deps/lib
 
 mkdir -p build && cd build
 
-wget https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz -O openssl-${OPENSSL_VERSION}.tar.gz
+wget --no-check-certificate https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz -O openssl-${OPENSSL_VERSION}.tar.gz
 tar -xzf openssl-${OPENSSL_VERSION}.tar.gz
 
 cd openssl-${OPENSSL_VERSION}
