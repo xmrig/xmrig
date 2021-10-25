f60350ed6c395ce6588f110ccb204859986de86c
MoneroOcean <support@moneroocean.stream>
2021-10-25 21:46:55 +0000

    https://github.com/MoneroOcean/xmrig/issues/70 fix

--- src/version.h
+++ src/version.h
@@ -22,7 +22,7 @@
 #define APP_ID        "xmrig"
 #define APP_NAME      "XMRig"
 #define APP_DESC      "XMRig miner"
-#define APP_VERSION   "6.15.2-mo2"
+#define APP_VERSION   "6.15.2-mo3"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
 #define APP_COPYRIGHT "Copyright (C) 2016-2021 xmrig.com"
