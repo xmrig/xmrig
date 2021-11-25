ecceba8ecd03e378f6c43e059af3e28187ad5f6d
XMRig <support@xmrig.com>
2021-11-25 17:44:36 +0700

    Add GhostRider support for AutoClient.

--- src/base/net/stratum/AutoClient.cpp
+++ src/base/net/stratum/AutoClient.cpp
@@ -50,7 +50,7 @@ bool xmrig::AutoClient::parseLogin(const rapidjson::Value &result, int *code)
     }
 
     const Algorithm algo(Json::getString(result, "algo"));
-    if (algo.family() != Algorithm::KAWPOW) {
+    if (algo.family() != Algorithm::KAWPOW && algo.family() != Algorithm::GHOSTRIDER) {
         *code = 6;
         return false;
     }
