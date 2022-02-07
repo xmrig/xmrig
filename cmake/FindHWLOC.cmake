634f9eba688126b949c03e03fecfe8c4cfff0495
Tony Butler <spudz76@gmail.com>
2022-02-07 13:07:10 -0700

    Check for required L3CACHE object type in hwloc

--- cmake/FindHWLOC.cmake
+++ cmake/FindHWLOC.cmake
@@ -23,3 +23,10 @@ set(HWLOC_INCLUDE_DIRS ${HWLOC_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(HWLOC DEFAULT_MSG HWLOC_LIBRARY HWLOC_INCLUDE_DIR)
+
+include(CheckCSourceCompiles)
+check_c_source_compiles( "#include <hwloc.h>\nint main(void) {hwloc_obj_t o; o->type = HWLOC_OBJ_L3CACHE; return 0;}" HAVE_HWLOC_OBJ_L3CACHE)
+if (NOT HAVE_HWLOC_OBJ_L3CACHE)
+    message(STATUS "HWLOC is too old (v2 minimum), some algorithms will not be built, may cause other problems")
+    set(WITH_GHOSTRIDER OFF CACHE BOOL "Enable GhostRider algorithm" FORCE)
+endif()
