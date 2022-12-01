21c0a9d3f1a38bf9d3feaa27e210130c0a57cf0f
AjunkDha <95839753+ajunkdha@users.noreply.github.com>
2022-12-02 03:17:21 +0800

    Create verushash.cmake

---
+++ cmake/verushash.cmake
@@ -0,0 +1,21 @@
+if (WITH_VERUSHASH)
+    add_definitions(/DXMRIG_ALGO_VERUSHASH)
+    set(GHOSTRIDER_LIBRARY verushash)
+
+    list(APPEND HEADERS_CRYPTO
+        src/crypto/verus/haraka.h
+        src/crypto/verus/haraka_portable.h
+        src/crypto/verus/verus_hash.h
+    )
+
+    list(APPEND SOURCES_CRYPTO
+        src/crypto/verus/haraka.c
+        src/crypto/verus/equi-stratum.c
+        src/crypto/verus/haraka_portable.c
+        src/crypto/verus/verus_hash.c
+    )
+
+else()
+    remove_definitions(/DXMRIG_ALGO_VERUSHASH)
+    set(VERUSHASH_LIBRARY "")
+endif()
