f0f6d1666c250e8b10fa274d9d4b19cc918eb80a
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2023-05-29 20:13:28 +0200

    fixup! Allow using system libfmt

--- cmake/fmt.cmake
+++ cmake/fmt.cmake
@@ -1,14 +1,13 @@
 # SPDX-FileCopyrightText: © 2023 Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-find_package(fmt QUIET)
 
-if(fmt_FOUND)
-    set(FMT_LIBRARY fmt::fmt)
-else()
-    message(STATUS "Using bundled fmt library")
+if(WITH_BUNDLED_FMT)
     add_library(fmt INTERFACE)
     target_sources(fmt INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/src/3rdparty/fmt/format.cc)
     target_include_directories(fmt INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/src/3rdparty/fmt)
+else()
     set(FMT_LIBRARY fmt)
+    find_package(fmt REQUIRED)
+    set(FMT_LIBRARY fmt::fmt)
 endif()
