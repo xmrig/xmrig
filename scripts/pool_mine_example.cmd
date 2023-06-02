af87369e4f15c256e247da54a72c0cfec45ddf68
SChernykh <sergey.v.chernykh@gmail.com>
2023-06-02 09:34:26 +0200

    Updated example scripts
    - Hashvault is top 1 pool now, so changed it to a smaller pool
    - node.xmr.to doesn't exist anymore

--- scripts/pool_mine_example.cmd
+++ scripts/pool_mine_example.cmd
@@ -16,5 +16,5 @@
 :: Smaller pools also often have smaller fees/payout limits.
 
 cd /d "%~dp0"
-xmrig.exe -o pool.hashvault.pro:3333 -u 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD -p x
+xmrig.exe -o xmrpool.eu:3333 -u 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD -p x
 pause
