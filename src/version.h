4bda6e054d6b2d5bc429e0266b813fe25dbe0f62
XMRig <support@xmrig.com>
2023-11-23 19:51:41 +0700

    v6.21.0-dev

--- src/version.h
+++ src/version.h
@@ -22,15 +22,15 @@
 #define APP_ID        "xmrig"
 #define APP_NAME      "XMRig"
 #define APP_DESC      "XMRig miner"
-#define APP_VERSION   "6.20.1-dev"
+#define APP_VERSION   "6.21.0-dev"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
 #define APP_COPYRIGHT "Copyright (C) 2016-2023 xmrig.com"
 #define APP_KIND      "miner"
 
 #define APP_VER_MAJOR  6
-#define APP_VER_MINOR  20
-#define APP_VER_PATCH  1
+#define APP_VER_MINOR  21
+#define APP_VER_PATCH  0
 
 #ifdef _MSC_VER
 #   if (_MSC_VER >= 1930)
