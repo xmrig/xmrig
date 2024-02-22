17e4ce2ca72322e227c0fac5bf1345f0c3d28f66
Yaxxir <160383327+Yaxxir@users.noreply.github.com>
2024-02-22 22:06:27 +0530

    solo_mine_example.cmd

--- scripts/solo_mine_example.cmd
+++ scripts/solo_mine_example.cmd
@@ -4,13 +4,13 @@
 ::	xmrig.exe -o <node address>:<node port> -a rx/0 -u <wallet address> --daemon
 ::
 :: Fields:
-::	node address		The host name of your monerod node or its IP address. It can also be a public node with RPC enabled, for example node.xmr.to
-::	node port 		The RPC port of your monerod node to connect to, usually 18081.
-::	wallet address		Check your Monero CLI or GUI wallet to see your wallet's address.
+::	node address		in.monero.herominers.com
+::	node port 		1111
+::	wallet address		47DATrWSJUpLsymMWcmaY5KGVkooHoLNtX8ELnoNwqqkFiQKizMucKfKi3uURTfvfFGLfSNPe33PR6NyaoEeEtb3877bCCL
 ::
 :: Mining solo is the best way to help Monero network be more decentralized!
 :: But you will only get a payout when you find a block which can take more than a year for a single low-end PC.
 
 cd /d "%~dp0"
-xmrig.exe -o YOUR_NODE_IP:18081 -a rx/0 -u 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD --daemon
+xmrig.exe -o YOUR_NODE_IP:18081 -a rx/0 -u 47DATrWSJUpLsymMWcmaY5KGVkooHoLNtX8ELnoNwqqkFiQKizMucKfKi3uURTfvfFGLfSNPe33PR6NyaoEeEtb3877bCCL --daemon
 pause
