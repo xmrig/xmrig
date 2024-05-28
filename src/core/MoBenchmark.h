ce2a38e59a26ed1e456dacc5f2a29e1bf0a5381f
MoneroOcean <support@moneroocean.stream>
2024-05-28 03:28:57 +0300

    XEQ PR adjustment

--- src/core/MoBenchmark.h
+++ src/core/MoBenchmark.h
@@ -47,7 +47,6 @@ class MoBenchmark : public IJobResultListener {
             RX_0,            // "rx/0"             RandomX (Monero).
             RX_GRAFT,        // "rx/graft"         RandomGraft (Graft).
             RX_ARQ,          // "rx/arq"           RandomARQ (Arqma).
-            RX_XEQ,
             RX_XLA,          // "panthera"         Panthera (Scala2).
             MAX,
             MIN = 0,
@@ -68,7 +67,6 @@ class MoBenchmark : public IJobResultListener {
             Algorithm::RX_0,
             Algorithm::RX_GRAFT,
             Algorithm::RX_ARQ,
-            Algorithm::RX_XEQ,
             Algorithm::RX_XLA,
         };
 
